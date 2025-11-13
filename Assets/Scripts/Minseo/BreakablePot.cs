c92ee26fd232cbaeea4f5443085cab0543ab73a5
Jeon, Minseo <minseo4565@snu.ac.kr>
2025-11-13 23:53:22 +0900

    simple sword

--- Assets/Scripts/Minseo/BreakablePot.cs
+++ Assets/Scripts/Minseo/BreakablePot.cs
@@ -24,20 +24,17 @@ public class BreakablePot : MonoBehaviour
     {
         isBroken = true;
 
-        // 깨진 프리팹 생성
         if (brokenPrefab != null)
         {
             var go = Instantiate(brokenPrefab, transform.position, transform.rotation);
             go.GetComponent<PotPiece>().SetRbParams(GetComponent<Rigidbody>().linearVelocity, GetComponent<Rigidbody>().angularVelocity);
         }
 
-        // 사운드 재생
         if (breakSound != null)
         {
             AudioSource.PlayClipAtPoint(breakSound, transform.position);
         }
 
-        // 원래 오브젝트 제거
         Destroy(gameObject);
     }
 }
